d5888fe3cb25ca0307b8ecb7450f1469f5fdab27
edgard <edgard@ubuntu-dev.myguest.virtualbox.org>
2025-09-18 23:32:51 -0300

    starting up

---
+++ purchase-management/src/main/java/net/mycompany/commerce/purchase_management/PurchaseManagementApplication.java
@@ -0,0 +1,13 @@
+package net.mycompany.commerce.purchase_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseManagementApplication.class, args);
+	}
+
+}
